de04a1b15cbd94500218925b462a7549f067797e
DelgadoElias <eliaslautarodelgado@gmail.com>
2022-09-23 23:21:43 -0300

    ADD initial-scaffolding

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Explore Windows</title>
+        <meta name="description" content="Windows Portfolio by DelgadoElias" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+          
+      </main>
+
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default Home
